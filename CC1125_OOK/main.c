db898a9fec0b8502f7404213f2be1d3bbe43094f
Hrithik <57284901+hrithik-ag@users.noreply.github.com>
2019-11-02 14:21:58 +0530

    Added .c and .h files
    .c and .h files required for the project

---
+++ CC1125_OOK/main.c
@@ -0,0 +1,18 @@
+/*
+ * CC1125_OOK.c
+ *
+ * Created: 02-11-2019 02:18:30 PM
+ * Author : Hrithik
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
